6442aa1df7ed2ae4e76f937da9d4c4a0cb8c951d
elfanhari <elfanhari88@gmail.com>
2024-02-01 21:44:03 +0700

    delete modal

--- resources/views/pages/auth/login2.blade.php
+++ resources/views/pages/auth/login2.blade.php
@@ -258,7 +258,6 @@
                 </div>
             </div>
         </div>
-        @yield('modal')
 
         <!-- Password toggle -->
         <script>
@@ -282,7 +281,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
     </script>
-    @yield('js')
 </body>
 
 </html>
